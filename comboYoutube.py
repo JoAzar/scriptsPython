3eefb7a107000105d510304198897f910ba4bb7b
Joel <90118139+JoAzar@users.noreply.github.com>
2023-12-28 10:43:06 -0300

    Update comboYoutube.py

--- comboYoutube.py
+++ comboYoutube.py
@@ -1,6 +1,6 @@
 import os
 
 import webbrowser 
-webbrowser.open_new_tab("https://www.youtube.com/watch?v=Gyq6XLRwblw")
-webbrowser.open_new_tab("https://www.twitch.tv/drops/inventory")
-webbrowser.open_new_tab("https://web.telegram.org/z/")
+webbrowser.open_new_tab("pagina1")
+webbrowser.open_new_tab("pagina2")
+webbrowser.open_new_tab("pagina3")

0da3eae9cadec4d421b5243dde4b8953bb565503
Joel <90118139+JoAzar@users.noreply.github.com>
2023-12-28 10:41:44 -0300

    Add files via upload

---
+++ comboYoutube.py
@@ -0,0 +1,6 @@
+import os
+
+import webbrowser 
+webbrowser.open_new_tab("https://www.youtube.com/watch?v=Gyq6XLRwblw")
+webbrowser.open_new_tab("https://www.twitch.tv/drops/inventory")
+webbrowser.open_new_tab("https://web.telegram.org/z/")
---
+++ mouseController.py
@@ -0,0 +1,7 @@
+import mouse
+
+#clic izquierdo
+
+mouse.click('left')
+mouse.get_position()
+print("esta mierda no anda en Linux...")
